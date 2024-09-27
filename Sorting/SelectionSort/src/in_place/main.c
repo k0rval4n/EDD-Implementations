3ab6c4d323bee020ff388302731d32370da34a29
k0rval4n <141952843+k0rval4n@users.noreply.github.com>
2024-09-27 17:54:54 -0300

    Feat: Implemented SelectionSort not in place and in place (#1)

---
+++ Sorting/SelectionSort/src/in_place/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "selection.h"
+
+
+int main() {
+    int array_size;
+    scanf("%d", &array_size);
+    int* array = calloc(array_size, sizeof(int));
+    for (int i = 0; i < array_size; i++) {
+        scanf("%d", &array[i]);
+    }
+    SelectionSort(array, array_size);
+    for (int i = 0; i < array_size; i++) {
+        printf("%d, ", array[i]);
+    }
+    free(array);
+    return 0;
+}
---
+++ Sorting/SelectionSort/src/in_place/selection.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+void SelectionSort(int* array, int array_size){
+    for (int i = 0; i <= array_size - 2; i++) {
+        int min_element_index = i;
+        for (int j = i + 1; j <= array_size - 1; j++) {
+            if (array[j] > array[min_element_index]) {
+                min_element_index = j;
+            }
+        }
+        int min = array[min_element_index];
+        array[min_element_index] = array[i];
+        array[i] = min;
+    }
+}

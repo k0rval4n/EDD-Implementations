58eeefa49193800ddf49bc4b2ed4195e1714fb08
k0rval4n <141952843+k0rval4n@users.noreply.github.com>
2024-09-27 19:00:09 -0300

    Implemented SelectionSort not in place and in place (#3)
    * Implemented Insertion Sort not in place
    
    * fix space before curly bracket of InsertionSort not in place
    
    * Implemented Insertion Sort in place

---
+++ Sorting/InsertionSort/src/not_in_place/insertion.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+
+int* InsertionSort(int* array, int array_size) {
+    int* sorted_array = calloc(array_size, sizeof(int));
+    int sorted_array_actual_size = 0;
+    for (int i = 0; i < array_size; i++) {
+        int element = array[i];
+        bool inserted = false;
+        for (int j = 0; j < sorted_array_actual_size; j++) {
+            if (element < sorted_array[j]) {
+                for (int k = sorted_array_actual_size; k > j; k--) {
+                    sorted_array[k] = sorted_array[k - 1];
+                }
+                sorted_array[j] = element;
+                inserted = true;
+                break;
+            }
+        }
+        if (!inserted) {
+            sorted_array[sorted_array_actual_size] = element;
+        }
+        sorted_array_actual_size++;
+    }
+    return sorted_array;
+}
